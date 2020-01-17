9b78a52e0edb50c296600dd52b34f600ae58c765
Dan Rasmussen <drasmussen49@gmail.com>
2020-01-17 14:19:33 -0500

    First commit. Have added OrganicPet class and unit test .cs file

---
+++ OrganicPets.Tests/OrganicPetsTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OrganicPets.Tests
+{
+    public class OrganicPetsTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

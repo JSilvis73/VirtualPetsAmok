777f4f733ee04b9776052cd9ee5a5ba30578fb81
joshuatreeee <joshuatreeee@gmail.com>
2020-01-24 14:18:40 -0500

    added tests for TakeAllPetsToDoctor and PlayWithAllPets

--- OrganicPets.Tests/OrganicPetTests.cs
+++ OrganicPets.Tests/OrganicPetTests.cs
@@ -117,13 +117,55 @@ namespace OrganicPets.Tests
             VirtualPetShelter petShelter = new VirtualPetShelter();
             petShelter.shelteredPets.Add(new OrganicPet());
             petShelter.shelteredPets.Add(new OrganicPet());
-            petShelter.shelteredPets.Add(new OrganicPet());
 
             petShelter.FeedAllPetsDinner();
 
-            Assert.Equal(0, petShelter.shelteredPets[0].HungerLevel);
             Assert.Equal(0, petShelter.shelteredPets[1].HungerLevel);
-            Assert.Equal(0, petShelter.shelteredPets[2].HungerLevel);
+
+        }
+        [Fact]
+        public void Does_TakeAllPetsToDoctor_Increase_All_HealthLevels_To_Ten()
+        {
+            VirtualPetShelter petShelter = new VirtualPetShelter();
+            petShelter.shelteredPets.Add(new OrganicPet());
+            petShelter.shelteredPets.Add(new OrganicPet());
+
+            petShelter.TakeAllPetsToDoctor();
+
+            Assert.Equal(10, petShelter.shelteredPets[1].HealthLevel);
+        }
+        [Fact]
+        public void Does_PlayWithAllPets_Increase_All_HungerLevels_By_2()
+        {
+            VirtualPetShelter petShelter = new VirtualPetShelter();
+            petShelter.shelteredPets.Add(new OrganicPet());
+            petShelter.shelteredPets.Add(new OrganicPet());
+
+            petShelter.PlayWithAllPets();
+
+            Assert.Equal(5, petShelter.shelteredPets[1].HungerLevel);
+        }
+        [Fact]
+        public void Does_PlayWithAllPets_Increase_All_HealthLevels_By_1()
+        {
+            VirtualPetShelter petShelter = new VirtualPetShelter();
+            petShelter.shelteredPets.Add(new OrganicPet());
+            petShelter.shelteredPets.Add(new OrganicPet());
+
+            petShelter.PlayWithAllPets();
+
+            Assert.Equal(8, petShelter.shelteredPets[1].HealthLevel);
+        }
+        [Fact]
+        public void Does_PlayWithAllPets_Decrease_All_BoredomLevels_By_2()
+        {
+            VirtualPetShelter petShelter = new VirtualPetShelter();
+            petShelter.shelteredPets.Add(new OrganicPet());
+            petShelter.shelteredPets.Add(new OrganicPet());
+
+            petShelter.PlayWithAllPets();
+
+            Assert.Equal(1, petShelter.shelteredPets[1].BoredomLevel);
         }
     }
 }
